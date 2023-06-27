31e91868d95dd74200828c8429122d5483957015
neo773 <62795688+neo773@users.noreply.github.com>
2023-06-27 21:50:48 +0530

    add dist for npm

---
+++ dist/index.d.ts
@@ -0,0 +1,87 @@
+type Type = "Residential" | "Wireless" | "Business" | "Hosting" | "TOR" | "SOCKS" | "SOCKS4" | "SOCKS4A" | "SOCKS5" | "SOCKS5H" | "Shadowsocks" | "HTTP" | "HTTPS" | "Compromised Server" | "Inference Engine" | "OpenVPN" | "VPN" | "whitelisted by x" | "blacklisted by x";
+type Status = "ok" | "warning" | "denied" | "error";
+export type IPAddressInfo = {
+    asn: string;
+    provider: string;
+    organisation: string;
+    continent: string;
+    continentcode: string;
+    country: string;
+    isocode: string;
+    region: string;
+    regioncode: string;
+    timezone: string;
+    city: string;
+    latitude: number;
+    longitude: number;
+    currency: {
+        code: string;
+        name: string;
+        symbol: string;
+    };
+    vpn: "yes" | "no";
+    proxy: "yes" | "no";
+    type: Type;
+    risk: number;
+};
+export type ProxyCheckResponse = {
+    status: Status;
+    node?: string;
+    query_time?: string;
+} & {
+    [ipAddress: string]: IPAddressInfo;
+};
+interface ProxyCheckConstructor {
+    api_key: string;
+}
+interface ProxyCheckOptions {
+    vpn?: 0 | 1 | 2 | 3;
+    asn?: 0 | 1;
+    node?: 0 | 1;
+    time?: 0 | 1;
+    inf?: 0 | 1;
+    risk?: 1 | 2;
+    port?: 0 | 1;
+    seen?: 0 | 1;
+    days?: 0 | 1;
+    tag?: string;
+}
+interface ProxyCheckGetUsageReturn {
+    error?: string;
+    burst_tokens_available: number;
+    burst_token_allowance: number;
+    burst_token_active: number;
+    queries_today: number;
+    daily_limit: number;
+    queries_total: number;
+    plan_tier: "Free";
+}
+interface ProxyCheckDetectionHistory {
+    time_formatted: string;
+    time_raw: string;
+    address: string;
+    country: string;
+    detection_type: string;
+    answering_node: string;
+    tag: string;
+}
+interface ProxyCheckQueryHistory {
+    proxies: string;
+    vpns: string;
+    undetected: string;
+    refused_queries: string;
+    total_queries: number;
+}
+interface ProxyCheckGetDetectionsOptions {
+    limit?: number;
+    offset?: number;
+}
+declare class ProxyCheck {
+    private api_key;
+    constructor({ api_key }: ProxyCheckConstructor);
+    checkIP(ip: string | string[], options?: ProxyCheckOptions): Promise<ProxyCheckResponse>;
+    getUsage(): Promise<ProxyCheckGetUsageReturn>;
+    getQueries(): Promise<ProxyCheckQueryHistory[]>;
+    getDetections(options?: ProxyCheckGetDetectionsOptions): Promise<ProxyCheckDetectionHistory[]>;
+}
+export default ProxyCheck;
---
+++ dist/index.js
@@ -0,0 +1,77 @@
+import fetch from "node-fetch";
+const BASE_URL = "https://proxycheck.io";
+class ProxyCheck {
+    api_key;
+    constructor({ api_key }) {
+        this.api_key = api_key;
+    }
+    async checkIP(ip, options = {}) {
+        const endpoint = Array.isArray(ip) ? "/v2/" : `/v2/${ip}`;
+        const url = new URL(`${BASE_URL}${endpoint}`);
+        url.searchParams.append("key", this.api_key);
+        for (const key in options) {
+            url.searchParams.append(key, options[key].toString());
+        }
+        if (Array.isArray(ip)) {
+            url.searchParams.append("ips", ip.join(","));
+            const response = await fetch(url.toString(), {
+                method: "POST",
+                body: url.searchParams,
+            });
+            return response.json();
+        }
+        else {
+            const response = await fetch(url.toString());
+            const data = await response.json();
+            return convertKeysToSnakeCase(data);
+        }
+    }
+    async getUsage() {
+        const url = `${BASE_URL}/dashboard/export/usage/?key=${this.api_key}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        return convertKeysToSnakeCase(data);
+    }
+    async getQueries() {
+        const url = `${BASE_URL}/dashboard/export/queries/?json=1&key=${this.api_key}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        return convertToArray(convertKeysToSnakeCase(data));
+    }
+    async getDetections(options = {}) {
+        const limit = options.limit ?? 100;
+        const offset = options.offset ?? 0;
+        const url = `${BASE_URL}/dashboard/export/detections/?json=1&key=${this.api_key}&limit=${limit}&offset=${offset}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        return convertToArray(convertKeysToSnakeCase(data));
+    }
+}
+const convertToArray = (obj) => {
+    const result = [];
+    for (const key in obj) {
+        result.push(obj[key]);
+    }
+    return result;
+};
+function convertKeysToSnakeCase(obj) {
+    const snakeCaseObj = {};
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            const snakeCaseKey = key.toLowerCase().replaceAll(" ", "_");
+            if (typeof obj[key] === "object" &&
+                obj[key] !== null &&
+                !Array.isArray(obj[key])) {
+                // @ts-ignore
+                snakeCaseObj[snakeCaseKey] = convertKeysToSnakeCase(obj[key]);
+            }
+            else {
+                // @ts-ignore
+                snakeCaseObj[snakeCaseKey] = obj[key];
+            }
+        }
+    }
+    // @ts-ignore
+    return snakeCaseObj;
+}
+export default ProxyCheck;
